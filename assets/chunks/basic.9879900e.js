8d5fa47938b80ce138edddf4c7cddf8925d3c012
vccyb <32866030+vccyb@users.noreply.github.com>
2023-04-01 14:54:16 +0000

    chore: setup my vue components

---
+++ assets/chunks/basic.9879900e.js
@@ -0,0 +1,1 @@
+import{_ as o,c as n,o as t}from"./framework.b4912763.js";const c={},e={};function r(s,a){return t(),n("div",null,"按钮")}typeof c=="function"&&c(e);const _=o(e,[["render",r]]);export{_ as default};
